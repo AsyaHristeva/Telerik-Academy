579737995f2f5f41f627274bb274e96da670df7e
Anastasiya Hristeva <aspartanida@gmail.com>
2015-08-02 07:26:03 +0300

    Update jQueryPlugin.js

--- jQueryPlugin.js
+++ jQueryPlugin.js
@@ -1,6 +1,8 @@
 function solve(){
   return function(selector){
-	 var index; 
+	 var index,
+	     selectors=$(selector).children(),
+	     len=selectors.length;
 	  
         $(selector).attr('id', 'the-select');
 	$('<div class="dropdown-list"></div>').appendTo('body');
@@ -8,11 +10,13 @@ function solve(){
 	.css("display", "none");
 	$('<div class="current" data-value="">Option 1</div>')
 	.appendTo('.dropdown-list');
-	$('<div class="options-container" style="position: absolute; display: none">').appendTo('.dropdown-list');
+	$('<div class="options-container" style="position: absolute; display: none">')
+	.appendTo('.dropdown-list');
 	
-	for(index=0;index<5;index+=1) {  
+	for(index=0;index<len;index+=1) {  
 		$(' <div class="dropdown-item" data-value="'+
-		(index-1)+'" data-index = "'+index+'">Option '+(index+1)+'</div>').appendTo('.options-container');
+		(index-1)+'" data-index = "'+index+'">'+selectors.eq(index).text()+'</div>')
+		.appendTo('.options-container');
 	}  
 	
 	$('.current').click(function(){
